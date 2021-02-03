4d1688f071f2b4e01fea400fcecc1473c602b1d9
Xiangxu Liu <xxliu@toughtworks.com>
2021-02-03 16:18:19 +0800

    add func for deck type

--- main.go
+++ main.go
@@ -1,13 +1,10 @@
 package main
 
-import "fmt"
-
 func main() {
 	cards := deck{"Ace of Diamonds", newCard()}
 	cards = append(cards, "Six of Spades")
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+
+	cards.print()
 }
 
 func newCard() string {
