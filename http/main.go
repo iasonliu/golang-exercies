10b5cfc41291731114ead73abbf8a829f90ce5c9
Xiangxu Liu <xxliu@toughtworks.com>
2021-02-07 18:32:58 +0800

    custom writer intfaces

--- http/main.go
+++ http/main.go
@@ -7,11 +7,18 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
 	resp, err := http.Get("https://www.google.com")
 	if err != nil {
 		fmt.Println("Error", err)
 		os.Exit(1)
 	}
-	io.Copy(os.Stdout, resp.Body)
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	return 1, nil
 }
