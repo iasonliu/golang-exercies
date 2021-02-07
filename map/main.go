e691bebe55cdfb8915831531b34b6ab0d1e9f108
Xiangxu Liu <xxliu@toughtworks.com>
2021-02-07 14:43:46 +0800

    loop maps

--- map/main.go
+++ map/main.go
@@ -6,17 +6,19 @@ import (
 
 func main() {
 	// var colors map[string]string
-	colors := make(map[string]string)
-	// colors := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"balck": "#000000",
-	// 	"green": "#ffffff",
-	// }
+	// colors := make(map[string]string)
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"balck": "#000000",
+		"green": "#ffffff",
+	}
 
 	// add key vaule to map
-	colors["red"] = "#ff0000"
-	colors["white"] = "#ffffff"
+	// colors["red"] = "#ff0000"
+	// colors["white"] = "#ffffff"
 	// delete key from map
-	delete(colors, "red")
-	fmt.Println(colors)
+	// delete(colors, "red")
+	for k, v := range colors {
+		fmt.Printf("key is %v, vaule is %v \n", k, v)
+	}
 }
