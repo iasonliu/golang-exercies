983d940f3ffeb8d3fc87d7b85c441d287cb899ae
Xiangxu Liu <xxliu@toughtworks.com>
2021-02-07 19:36:42 +0800

    add exit 1

--- readfile/main.go
+++ readfile/main.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	if len(os.Args) < 2 {
 		log.Fatal("pls add args as filename")
+		os.Exit(1)
 	}
 	filename := os.Args[1]
 	readfile(filename)
@@ -18,8 +19,10 @@ func readfile(filename string) {
 	file, err := os.Open(filename)
 	if err != nil {
 		log.Fatal(err)
+		os.Exit(1)
 	}
 	if _, err := io.Copy(os.Stdout, file); err != nil {
 		log.Fatal(err)
+		os.Exit(1)
 	}
 }
