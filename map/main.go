9d0c3a5f31b8cc1e65cdba2b65d70bf2070ef6f2
Xiangxu Liu <xxliu@toughtworks.com>
2021-02-07 14:40:58 +0800

    add maps

---
+++ map/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// var colors map[string]string
+	colors := make(map[string]string)
+	// colors := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"balck": "#000000",
+	// 	"green": "#ffffff",
+	// }
+
+	// add key vaule to map
+	colors["red"] = "#ff0000"
+	colors["white"] = "#ffffff"
+	// delete key from map
+	delete(colors, "red")
+	fmt.Println(colors)
+}
