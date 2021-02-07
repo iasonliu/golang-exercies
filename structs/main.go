c4736a2c48032dcafa18c1613b9cd2bd97a3ad7d
Xiangxu Liu <xxliu@toughtworks.com>
2021-02-07 11:51:57 +0800

    other way to struct ref to struct

--- structs/main.go
+++ structs/main.go
@@ -5,7 +5,7 @@ import "fmt"
 type person struct {
 	firstName string
 	lastName  string
-	contact   contactInfo
+	contactInfo
 }
 
 type contactInfo struct {
@@ -17,7 +17,7 @@ func main() {
 	jim := person{
 		firstName: "Jim",
 		lastName:  "Party",
-		contact: contactInfo{
+		contactInfo: contactInfo{
 			email:   "xxx@email.com",
 			zipCode: 123112,
 		},
