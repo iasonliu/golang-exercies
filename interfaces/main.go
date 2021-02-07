9da4e0e4eda61c49c350e825ecd307aa15496cf1
Xiangxu Liu <xxliu@toughtworks.com>
2021-02-07 16:03:46 +0800

    add interfaces

--- interfaces/main.go
+++ interfaces/main.go
@@ -2,35 +2,32 @@ package main
 
 import "fmt"
 
+type bot interface {
+	getGreeting() string
+}
 type englishBot struct {
-	name string
 }
 
 type spanishBot struct {
-	name string
-}
-
-func (eb *englishBot) getGreeting() string {
-	// VERY custom logic for generating an english greeting
-	return "Hello " + eb.name + "!"
-}
-
-func (sb *spanishBot) getGreeting() string {
-	// VERY custom logic for generating an spanish greeting
-	return "Hola " + sb.name + "!"
-}
-
-func ebPrintGreeting(eb englishBot) {
-	fmt.Println(eb.getGreeting())
-}
-
-func sbPrintGreeting(sb spanishBot) {
-	fmt.Println(sb.getGreeting())
 }
 
 func main() {
-	eb := englishBot{name: "eb"}
-	ebPrintGreeting(eb)
-	sb := spanishBot{name: "sb"}
-	sbPrintGreeting(sb)
+	eb := englishBot{}
+	printGreeting(eb)
+	sb := spanishBot{}
+	printGreeting(sb)
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+func (englishBot) getGreeting() string {
+	// VERY custom logic for generating an english greeting
+	return "Hello "
+}
+
+func (spanishBot) getGreeting() string {
+	// VERY custom logic for generating an spanish greeting
+	return "Hola "
 }
