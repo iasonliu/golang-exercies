ad36b75ba05e932e1bd31b00d917405358cbdaea
Xiangxu Liu <xxliu@toughtworks.com>
2021-02-08 16:40:44 +0800

    one way using channels

--- channels/main.go
+++ channels/main.go
@@ -14,15 +14,23 @@ func main() {
 		"https://stackoverflow.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
+	}
+
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-c)
 	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	if _, err := http.Get(link); err != nil {
 		fmt.Println(link, "might be down!")
+		c <- "Migth be down I think" + link
 		return
 	}
 	fmt.Println(link, "is up!")
+	c <- "Yep it's up! " + link
 }
