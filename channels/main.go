153edbf15a6dd6e70220c47312cb8c877e8358be
Xiangxu Liu <xxliu@toughtworks.com>
2021-02-08 17:42:32 +0800

    fix func go var

--- channels/main.go
+++ channels/main.go
@@ -22,8 +22,10 @@ func main() {
 	}
 
 	for l := range c {
-		time.Sleep(5 * time.Second)
-		go checkLink(l, c)
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l)
 	}
 }
 
