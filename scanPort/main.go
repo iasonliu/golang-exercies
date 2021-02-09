d771a2687803c80f88af8b938da0067c43473286
Xiangxu Liu <xxliu@toughtworks.com>
2021-02-09 09:41:28 +0800

    change nmap to scanport

--- scanPort/main.go
+++ scanPort/main.go
@@ -2,24 +2,26 @@ package main
 
 import (
 	"fmt"
-	"net"
 	"sync"
 )
 
 func main() {
+	ports := make(chan int, 100)
 	var wg sync.WaitGroup
+	for i := 0; i < cap(ports); i++ {
+		go worker(ports, &wg)
+	}
 	for i := 1; i <= 1024; i++ {
 		wg.Add(1)
-		go func(port int) {
-			defer wg.Done()
-			address := fmt.Sprintf("scanme.nmap.org:%d", port)
-			conn, err := net.Dial("tcp", address)
-			if err != nil {
-				return
-			}
-			conn.Close()
-			fmt.Printf("%d open\n", port)
-		}(i)
+		ports <- i
 	}
 	wg.Wait()
+	close(ports)
+}
+
+func worker(ports chan int, wg *sync.WaitGroup) {
+	for p := range ports {
+		fmt.Println(p)
+		wg.Done()
+	}
 }
