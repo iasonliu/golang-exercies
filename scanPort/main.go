4a796b1f3a844dcf344c913463f760ecf44d1e23
Xiangxu Liu <xxliu@toughtworks.com>
2021-02-09 10:02:00 +0800

    worker mode to sync scanport

--- scanPort/main.go
+++ scanPort/main.go
@@ -2,26 +2,47 @@ package main
 
 import (
 	"fmt"
-	"sync"
+	"net"
+	"sort"
 )
 
 func main() {
 	ports := make(chan int, 100)
-	var wg sync.WaitGroup
+	results := make(chan int)
+	var openPorts []int
 	for i := 0; i < cap(ports); i++ {
-		go worker(ports, &wg)
+		go worker(ports, results)
 	}
-	for i := 1; i <= 1024; i++ {
-		wg.Add(1)
-		ports <- i
+
+	go func() {
+		for i := 1; i <= 1024; i++ {
+			ports <- i
+		}
+	}()
+
+	for i := 0; i < 1024; i++ {
+		port := <-results
+		if port != 0 {
+			openPorts = append(openPorts, port)
+		}
 	}
-	wg.Wait()
 	close(ports)
+	close(results)
+	sort.Ints(openPorts)
+	for _, port := range openPorts {
+		fmt.Printf("%d open\n", port)
+	}
 }
 
-func worker(ports chan int, wg *sync.WaitGroup) {
+func worker(ports chan int, results chan int) {
 	for p := range ports {
-		fmt.Println(p)
-		wg.Done()
+		address := fmt.Sprintf("scanme.nmap.org:%d", p)
+		conn, err := net.Dial("tcp", address)
+		if err != nil {
+			results <- 0
+			continue
+		}
+		conn.Close()
+		results <- p
 	}
 }
