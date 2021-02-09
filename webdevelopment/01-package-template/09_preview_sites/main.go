c393c0acd91b89f9d68cf2273b54c513777e9a0c
Xiangxu Liu <xxliu@toughtworks.com>
2021-02-09 21:23:47 +0800

    add func map for template

---
+++ webdevelopment/01-package-template/09_preview_sites/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.tmpl"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("public"))))
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(res http.ResponseWriter, req *http.Request) {
+	err := tpl.ExecuteTemplate(res, "index.tmpl", nil)
+	if err != nil {
+		log.Fatalln("template didn't execute:", err)
+	}
+}
