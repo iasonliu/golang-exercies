69c760954cab7985d3c42a94211294fa15521836
Xiangxu Liu <xxliu@toughtworks.com>
2021-02-25 14:26:21 +0800

    using channels

--- concurrency/00/README.md
+++ concurrency/00/README.md
@@ -1,2 +1,16 @@
 https://www.youtube.com/watch?v=f6kdp27TYZs
 https://talks.golang.org/2012/concurrency.slide
+
+# Channels
+A channel in Go provides a connection between two goroutines, allowing them to communicate.
+
+    // Declaring and initializing.
+    var c chan int
+    c = make(chan int)
+    // or
+    c := make(chan int)
+    // Sending on a channel.
+    c <- 1
+    // Receiving from a channel.
+    // The "arrow" indicates the direction of data flow.
+    value = <-c
--- concurrency/00/main.go
+++ concurrency/00/main.go
@@ -7,15 +7,19 @@ import (
 )
 
 func main() {
-	go boring("boring!")
-	fmt.Println("I'm listening")
-	time.Sleep(2 * time.Second)
+	c := make(chan string)
+	go boring("boring!", c)
+	for i := 0; i < 5; i++ {
+		// Receive experssion is just a value
+		fmt.Printf("You say: %q\n", <-c)
+	}
 	fmt.Println("You're boring; I'm Leaveing")
 }
 
-func boring(msg string) {
+func boring(msg string, c chan string) {
 	for i := 0; ; i++ {
-		fmt.Println(msg, i)
+		// Expression to be sent can be any suiable value
+		c <- fmt.Sprintf("%s %d", msg, i)
 		time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
 	}
 }
