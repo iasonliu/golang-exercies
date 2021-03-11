be4e27116f777795ec3c0789c8faf32c02bf2b8d
Xiangxu Liu <xxliu@toughtworks.com>
2021-03-11 11:32:07 +0800

    add login

--- web-authentication/hands-on/1/main.go
+++ web-authentication/hands-on/1/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"net/url"
 
@@ -11,12 +12,21 @@ import (
 
 type User struct {
 	Email    string
-	Passwrod []byte
+	Password []byte
 }
 
 type Users []User
 
 var UserList Users
+
+func getUser(email string) (User, error) {
+	for _, u := range UserList {
+		if u.Email == email {
+			return u, nil
+		}
+	}
+	return User{}, fmt.Errorf("User not Find")
+}
 
 func greet(w http.ResponseWriter, r *http.Request) {
 	errMsg := r.FormValue("errormsg")
@@ -27,7 +37,7 @@ func greet(w http.ResponseWriter, r *http.Request) {
 		<meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>JWT token Example</title>
+		<title>Web Example</title>
 	</head>
 	<body>
 	<h1>IF THERE IS ANY ERROR: ` + errMsg + `</h1>
@@ -63,9 +73,56 @@ func register(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func login(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		userEmail := r.FormValue("emailForm")
+		userPassword := r.FormValue("passwordForm")
+		if userEmail == "" || userPassword == "" {
+			errorMsg := url.QueryEscape("Your Email or Password empty")
+			http.Redirect(w, r, "/?errormsg="+errorMsg, http.StatusSeeOther)
+			return
+		}
+		u, err := getUser(userEmail)
+		log.Printf("%#v", u)
+		if err != nil {
+			errorMsg := url.QueryEscape("User Not find")
+			http.Redirect(w, r, "/login?errormsg="+errorMsg, http.StatusSeeOther)
+			return
+		}
+		err = bcrypt.CompareHashAndPassword(u.Password, []byte(userPassword))
+		if err != nil {
+			errorMsg := url.QueryEscape("Username or Password error")
+			http.Redirect(w, r, "/login?errormsg="+errorMsg, http.StatusSeeOther)
+			return
+		}
+		fmt.Fprintf(w, "Login %s", u.Email)
+		return
+	}
+	errMsg := r.FormValue("errormsg")
+
+	html := `<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Web Example</title>
+	</head>
+	<body>
+	<h1>IF THERE IS ANY ERROR: ` + errMsg + `</h1>
+	<form action="/login" method="post">
+		<input type="email" name="emailForm"/>
+		<input type="password" name="passwordForm"/>
+		<input type="submit" name="Login"/>
+	</form>
+	</body>
+	</html>`
+	io.WriteString(w, html)
+}
 func main() {
 
 	http.HandleFunc("/", greet)
 	http.HandleFunc("/register", register)
+	http.HandleFunc("/login", login)
 	http.ListenAndServe(":8080", nil)
 }
