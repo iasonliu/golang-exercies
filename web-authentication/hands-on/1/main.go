84e322db33ed3a5e8e2d94f7c2532bc8d4b89c46
Xiangxu Liu <xxliu@toughtworks.com>
2021-03-11 10:30:47 +0800

    add handson 1

---
+++ web-authentication/hands-on/1/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	Email    string
+	Passwrod []byte
+}
+
+type Users []User
+
+var UserList Users
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	errMsg := r.FormValue("errormsg")
+
+	html := `<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>JWT token Example</title>
+	</head>
+	<body>
+	<h1>IF THERE IS ANY ERROR: ` + errMsg + `</h1>
+	<form action="/register" method="post">
+		<input type="email" name="emailForm"/>
+		<input type="password" name="passwordForm"/>
+		<input type="submit" name="register"/>
+	</form>
+	</body>
+	</html>`
+	io.WriteString(w, html)
+}
+
+func register(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		userEmail := r.FormValue("emailForm")
+		userPassword := r.FormValue("passwordForm")
+		if userEmail == "" || userPassword == "" {
+			errorMsg := url.QueryEscape("Your Email or Password empty")
+			http.Redirect(w, r, "/?errormsg="+errorMsg, http.StatusSeeOther)
+		}
+		hashPassword, err := bcrypt.GenerateFromPassword([]byte(userPassword), bcrypt.DefaultCost)
+		if err != nil {
+			errorMsg := url.QueryEscape("there was Internal server Error!!")
+			http.Error(w, errorMsg, http.StatusInternalServerError)
+		}
+		UserList = append(UserList, User{userEmail, hashPassword})
+		fmt.Fprintf(w, "%#v", UserList)
+	} else {
+		errorMsg := url.QueryEscape("Your method was not Post")
+		http.Redirect(w, r, "/?errormsg="+errorMsg, http.StatusSeeOther)
+		return
+	}
+}
+
+func main() {
+
+	http.HandleFunc("/", greet)
+	http.HandleFunc("/register", register)
+	http.ListenAndServe(":8080", nil)
+}
